b6f4b6e7e6e5a43faf08a94462700ec5b5abd097
onocmouse <onocmouse@gmail.com>
2016-04-06 12:06:24 -0500

    Redis!

--- index.js
+++ index.js
@@ -3,21 +3,26 @@ var twitter = require('twode');
 var BotEnvironment = require('twitter-bot-environment');
 var path = require('path');
 var fs = require('fs');
+var redis = require('redis');
 
 var environment = new BotEnvironment();
 
 function Bot(handle) {
 	this.handle = handle.charAt(0) === '@' ? handle : '@' + handle;
 	this.twitter = new twitter(environment.getEnvironment());
+	this.sentenceQueue = null;
+	this.redis_client = redis.createClient();
+	this.redis_client.get('sentence_queue', (err, reply) => {
+		if(reply == null) {
+			this.sentenceQueue = [];
+		} else {
+			this.sentenceQueue = JSON.parse(reply);
+		}
+	});
 }
 
 Bot.prototype.truncate_tweet = function(tweet) {
 	return (tweet.length > 140) ? _.truncate(tweet, {length: 140, omission: '…'}) : tweet;
-	try {
-		this.sentenceQueue = JSON.parse(fs.readFileSync('./queue.json').toString());
-	} catch(err) {
-		this.sentenceQueue = [];
-	}
 }
 
 Bot.prototype.generateSentences = function() {
@@ -31,16 +36,17 @@ Bot.prototype.generateSentences = function() {
 }
 
 Bot.prototype.run = function() {
-	if(this.sentenceQueue === undefined || this.sentenceQueue === null || this.sentenceQueue.length == 0) {
+	if(this.sentenceQueue === undefined || this.sentenceQueue === null) {
+		setTimeout(this.run.bind(this), 100);
+		return;
+	}
+	if(this.sentenceQueue.length == 0) {
 		this.sentenceQueue = this.generateSentences();
 	}
 	sentence = this.sentenceQueue.pop();
-	fs.writeFile('./queue.json', JSON.stringify(this.sentenceQueue, null, 4), function(err) {
-		if(err) {
-			console.log(err);
-		}
-	});
+	this.redis_client.set('sentence_queue', JSON.stringify(this.sentenceQueue));
 	console.log(this.truncate_tweet(sentence));
+	bot.redis_client.end(true);
 }
 
 var bot = new Bot('@quintilian_bot');
